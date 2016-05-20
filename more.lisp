73a16d95ba0826dfa1fa9986acb0903249b2dbb5
Max Rottenkolber <max@mr.gy>
2016-05-20 22:23:23 +0200

    Special-case =DESTRUCTURE.

--- more.lisp
+++ more.lisp
@@ -16,6 +16,31 @@
 (defun ?eq (x &optional (parser (=element)))
   (?test parser 'eq x))
 
+(defun =list-form-p (form)
+  (and (listp form) (eq (first form) '=list)))
+
+;; This is a “primitive” strictly speaking.
+(defun special-case-=list (bindings ignorable-syms parsers body
+                           &aux (input-sym (gensym "input"))
+                                (rest-sym (gensym "rest"))
+                                (value-sym (gensym "value"))
+                                (df-sym (gensym "df")))
+  `(let ((,df-sym (lambda ,bindings
+                    (declare (ignore ,@ignorable-syms))
+                    ,@body)))
+     (lambda (,input-sym)
+       (block =destructure-=list
+         (let ((,value-sym
+                (funcall ,df-sym
+                         ,@(loop for parser in parsers collect
+                                `(multiple-value-bind (,rest-sym ,value-sym)
+                                     (funcall ,parser ,input-sym)
+                                   (unless ,rest-sym
+                                     (return-from =destructure-=list))
+                                   (setf ,input-sym ,rest-sym)
+                                   ,value-sym)))))
+         (values ,input-sym ,value-sym t))))))
+
 (defmacro =destructure ((&rest bindings) parser &body forms
                         &aux (result-sym (gensym "result")))
   (let* ((ignorable-syms)
@@ -26,8 +51,10 @@
          (special-syms (list* '&optional '&rest '&key '&allow-other-keys '&aux
                               ignorable-syms))
          (body (or forms (set-difference bindings special-syms))))
-    `(=transform ,parser
-                 (lambda (,result-sym)
-                   (destructuring-bind ,bindings ,result-sym
-                     (declare (ignore ,@ignorable-syms))
-                     ,@body)))))
+    (if (=list-form-p parser)
+        (special-case-=list bindings ignorable-syms (rest parser) body)
+        `(=transform ,parser
+                     (lambda (,result-sym)
+                       (destructuring-bind ,bindings ,result-sym
+                         (declare (ignore ,@ignorable-syms))
+                         ,@body))))))
