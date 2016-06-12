ab18de1dafab844fee5f05f9947b312cead00d12
Max Rottenkolber <max@mr.gy>
2016-06-12 17:44:42 +0200

    =destructure: maintain implicit body forms ordering.

--- more.lisp
+++ more.lisp
@@ -20,18 +20,19 @@
 (defun ?eq (x &optional (parser (=element)))
   (?test ('eq x) parser))
 
-(defmacro =destructure ((&rest bindings) parser &body forms
+(defmacro =destructure ((&rest lambda-list) parser &body forms
                         &aux (result-sym (gensym "result")))
   (let* ((ignorable-syms)
-         (bindings (loop for binding in bindings collect
-                        (if (string= "_" (symbol-name binding))
-                            (car (push (gensym "_") ignorable-syms))
-                            binding)))
+         (lambda-list (loop for symbol in lambda-list collect
+                           (if (string= "_" (symbol-name symbol))
+                               (car (push (gensym "_") ignorable-syms))
+                               symbol)))
          (special-syms (list* '&optional '&rest '&key '&allow-other-keys '&aux
                               ignorable-syms))
-         (body (or forms (set-difference bindings special-syms))))
+         (body (or forms (remove-if (lambda (x) (member x special-syms))
+                                    lambda-list))))
     `(=transform ,parser
                  (lambda (,result-sym)
-                   (destructuring-bind ,bindings ,result-sym
+                   (destructuring-bind ,lambda-list ,result-sym
                      (declare (ignore ,@ignorable-syms))
                      ,@body)))))
