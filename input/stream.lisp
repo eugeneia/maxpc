cde6a5ed9ec83adf1f1bbcaadff284dcc2f9c148
Max Rottenkolber <max@mr.gy>
2016-07-09 19:23:49 +0200

    input/stream: properly support unbounded streams.

--- input/stream.lisp
+++ input/stream.lisp
@@ -6,16 +6,27 @@
 
 (defparameter *chunk-size* (* 5 1000 1000)) ; 5 Mega
 
-(defun fill-buffer (buffer stream)
-  (let* ((old-buffer-size (length buffer))
-         (new-buffer-size (+ old-buffer-size *chunk-size*)))
-    (adjust-array buffer new-buffer-size)
-    (setf (fill-pointer buffer) new-buffer-size)
-    (setf (fill-pointer buffer) (read-sequence buffer stream
-                                               :start old-buffer-size))))
+(defgeneric fill-buffer (buffer stream))
 
-(defun eofp (stream)
-  (eq (peek-char nil stream nil 'eof) 'eof))
+(defmethod fill-buffer ((buffer array) (stream stream))
+  (handler-case (vector-push-extend
+                 (ecase (stream-element-type stream)
+                   (character (read-char stream))
+                   (signed-byte (read-byte stream)))
+                 buffer
+                 (the fixnum *chunk-size*))
+    (end-of-file (error) (declare (ignore error)))))
+
+(defmethod fill-buffer ((buffer array) (stream file-stream))
+  (let* ((file-length (file-length stream))
+         (old-buffer-size (length buffer))
+         (new-buffer-size (min (+ old-buffer-size *chunk-size*) file-length)))
+    (unless (= file-length old-buffer-size)
+      (adjust-array buffer new-buffer-size)
+      (setf (fill-pointer buffer)
+            new-buffer-size)
+      (setf (fill-pointer buffer)
+            (read-sequence buffer stream :start old-buffer-size)))))
 
 (defmethod make-input ((input-source stream))
   (let ((buffer (make-array 0
@@ -38,13 +49,13 @@
 (defmethod input-rest  ((input index-stream))
   (declare (optimize (speed 3) (debug 0) (safety 0)))
   (with-slots (position buffer stream) input
-    (unless (or (< (the position position)
-                   (1- (the position (length (the array buffer)))))
-                (eofp stream))
-      (fill-buffer buffer stream))
-    (make-index-stream :stream (the stream stream)
-                       :buffer (the array buffer)
-                       :position (1+ (the position position)))))
+    (let ((next-position (1+ (the position position))))
+      (unless (< (the position next-position)
+                 (the position (length (the array buffer))))
+        (fill-buffer buffer stream))
+      (make-index-stream :stream (the stream stream)
+                         :buffer (the array buffer)
+                         :position (the position next-position)))))
 
 (defmethod input-element-type ((input index-stream))
   (stream-element-type (the stream (index-stream-stream input))))
@@ -52,10 +63,6 @@
 (defmethod input-sequence ((input index-stream) (length fixnum))
   (declare (optimize (speed 3) (debug 0) (safety 0)))
   (with-slots (position buffer stream) input
-    (loop while (and (< (the position (length buffer))
-                        (+ (the position position) length))
-                     (not (eofp stream))) do
-         (fill-buffer buffer stream))
     (make-array length
                 :element-type (stream-element-type (the stream stream))
                 :displaced-to (the array buffer)
