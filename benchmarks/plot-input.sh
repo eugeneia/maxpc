ca8fbe6dadb6c517ee380575b411c8e7ee44bd36
Max Rottenkolber <max@mr.gy>
2016-08-03 16:11:06 +0200

    Plot a little smaller.

--- benchmarks/plot-input.sh
+++ benchmarks/plot-input.sh
@@ -4,7 +4,7 @@ source=input-Intel-Xeon-CPU-E31245-@-3.30GHz.dat
 
 gnuplot <<EOF
 set terminal pngcairo \
-size 2000,800 font 'Source Sans Pro,28' lw 3 crop
+size 1200,480 font 'Source Sans Pro,16.8' lw 1.8 crop
 set output 'input.png'
 set key outside
 set xtics ("100KB" 100000, "1MB" 1000000, "2MB" 2000000)
@@ -12,8 +12,8 @@ set xlabel "Input Size"
 unset ytics
 set ylabel "Time (s)"
 
-plot "$source" using 1:2 title "list" with linespoints lw 2 ps 3, \
-     "$source" using 1:3 title "array" with linespoints lw 2 ps 3, \
-     "$source" using 1:4 title "stream" with linespoints lw 2 ps 3, \
-     "$source" using 1:5 title "file-stream" with linespoints lw 2 ps 3
+plot "$source" using 1:2 title "list" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:3 title "array" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:4 title "stream" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:5 title "file-stream" with linespoints lw 1.2 ps 1.8
 EOF
--- benchmarks/plot-sexp.sh
+++ benchmarks/plot-sexp.sh
@@ -4,13 +4,13 @@ source=sexp-Intel-Xeon-CPU-E31245-@-3.30GHz.dat
 
 gnuplot <<EOF
 set terminal pngcairo \
-size 2000,800 font 'Source Sans Pro,28' lw 3 crop
+size 1200,480 font 'Source Sans Pro,16.8' lw 1.8 crop
 set output 'sexp.png'
 set key outside
 set xlabel "Input Size (B)"
 set ylabel "Time (s)"
-plot "$source" using 1:2 title "read-from-string" with linespoints lw 2 ps 3, \
-     "$source" using 1:3 title "MaxPC" with linespoints lw 2 ps 3, \
-     "$source" using 1:4 title "MPC" with linespoints lw 2 ps 3, \
-     "$source" using 1:5 title "Esrap" with linespoints lw 2 ps 3
+plot "$source" using 1:2 title "read-from-string" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:3 title "MaxPC" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:4 title "MPC" with linespoints lw 1.2 ps 1.8, \
+     "$source" using 1:5 title "Esrap" with linespoints lw 1.2 ps 1.8
 EOF
